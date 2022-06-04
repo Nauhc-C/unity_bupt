b23e8d05f0f8bf64b4151b587df8048a34ff9dc7
Nauhc-C <76831634+Nauhc-C@users.noreply.github.com>
2022-06-04 14:58:31 +0800

    完善第一人称 移动 跳跃等

---
+++ Assets/scripts/2 Controller/PlayerController.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private CharacterController cc;
+    public float moveSpeed;
+    public float jumpSpeed;
+    private float horizontalMove, verticalMove;
+    private Vector3 dir;
+    public float gravity;
+    public Vector3 velocity;
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float checkRadius;
+    public bool isGround;
+
+    private void Start()
+    {
+        cc = GetComponent<CharacterController>();
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGround = Physics.CheckSphere(groundCheck.position, checkRadius, groundLayer);
+
+        if(isGround && velocity.y < 0)
+        {
+            velocity.y = -1f;
+        }
+
+        horizontalMove = Input.GetAxis("Horizontal") * moveSpeed;
+        verticalMove = Input.GetAxis("Vertical") * moveSpeed;
+
+        dir = transform.forward * verticalMove + transform.right * horizontalMove;
+        cc.Move(dir * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            print("jump");
+            velocity.y = jumpSpeed;
+        }
+
+        velocity.y -= gravity * Time.deltaTime;
+        cc.Move(velocity * Time.deltaTime);
+    }
+
+}
--- Assets/scripts/2 Controller/cameraController.cs
+++ Assets/scripts/2 Controller/cameraController.cs
@@ -9,12 +9,17 @@ public class cameraController : MonoBehaviour
     public float mouseX, mouseY;
     public float mouseSensitivity;
 
+    public float xRotation;
+
     private void Update()
     {
         mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
         mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -70, 70);
+
         Player.transform.Rotate(Vector3.up * mouseX);
-        transform.localRotation = Quaternion.Euler(-mouseY, 0, 0);
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
     }
 }
