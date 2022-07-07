50588d8c0eec0c783cb5e3ca516e5e826b0d0d10
Nauhc-C <76831634+Nauhc-C@users.noreply.github.com>
2022-07-07 17:44:34 +0800

    添加角色自定义

---
+++ Assets/scripts/unity_chan.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class unity_chan : MonoBehaviour
+{
+    //皮肤渲染
+    private SkinnedMeshRenderer _mSkinnedMeshRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //获取组件
+        _mSkinnedMeshRenderer = GameObject.Find("hair_frontside").GetComponent<SkinnedMeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //改变颜色，这里使用了Color.Lerp()一个颜色的渐变
+        _mSkinnedMeshRenderer.material.color = Color.blue;
+            
+        print(_mSkinnedMeshRenderer.material.color);
+    }
+
+}
